44f1ac146761a10dfb536196d5f894138d59702e
Cheng <mazeyqian@gmail.com>
2026-04-13 00:05:50 +0800

    feat(list): convert to copyright

--- src/list.js
+++ src/list.js
@@ -1,1 +1,24 @@
-console.log("list");
+import { addStyle, genStyleString } from "mazey";
+
+const footerStyle = genStyleString(
+  ".footer>.hope-stack>a:first-child",
+  [ "display: none" ]
+)
+addStyle(footerStyle, { id: "polestar-list-footer" })
+
+const currentYear = new Date().getFullYear();
+const footerDivContent = genStyleString(
+  ".footer>.hope-stack:before",
+  [
+    `content: 'Copyright © ${currentYear} List'`,
+    "color: inherit",
+    "padding-inline-start: var(--hope-space-1_5)",
+    "padding-inline-end: var(--hope-space-1_5)",
+    "padding-top: var(--hope-space-1)",
+    "padding-bottom: var(--hope-space-1)",
+  ]
+)
+addStyle(footerDivContent, { id: "polestar-list-div-content" })
+
+console.log("footerStyle", footerStyle);
+console.log("footerDivContent", footerDivContent);
