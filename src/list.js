8abf9d01d0d800c0fd1886b9dac48eebcfea21d1
Cheng <mazeyqian@gmail.com>
2026-04-14 23:13:40 +0800

    feat(list): optimise hide functions

--- src/list.js
+++ src/list.js
@@ -24,23 +24,22 @@ const footerDivStyle = genStyleString(
 );
 addStyle(listHideStyle + footerDivStyle, { id: "polestar-list" });
 
-let hideGuestBtnIndex = 0;
-const hideGuestBtn = () => {
-  listCon.log(`run hideGuestBtn() with the index: ${hideGuestBtnIndex}`);
-  const browseGuestBtn = document.evaluate(
-    "//button[normalize-space(text())='Browse as a guest' or normalize-space(text())='以游客身份浏览']",
-    document,
-    null,
-    XPathResult.FIRST_ORDERED_NODE_TYPE,
-    null
-  ).singleNodeValue;
-  if (browseGuestBtn && browseGuestBtn.style) {
-    browseGuestBtn.style.display = 'none';
-    if (hideGuestBtnIndex) clearInterval(hideGuestBtnIndex);
-  }
-}
-
-window.addEventListener("load", () => {
+const timeToHideGuestBtn = () => {
+  let hideGuestBtnIndex = 0;
+  const hideGuestBtn = () => {
+    listCon.log(`run hideGuestBtn() with the index: ${hideGuestBtnIndex}`);
+    const browseGuestBtn = document.evaluate(
+      "//button[normalize-space(text())='Browse as a guest' or normalize-space(text())='以游客身份浏览']",
+      document,
+      null,
+      XPathResult.FIRST_ORDERED_NODE_TYPE,
+      null
+    ).singleNodeValue;
+    if (browseGuestBtn && browseGuestBtn.style) {
+      browseGuestBtn.style.display = 'none';
+      if (hideGuestBtnIndex) clearInterval(hideGuestBtnIndex);
+    }
+  };
   if (location.href.includes("@login")) {
     hideGuestBtnIndex = setInterval(hideGuestBtn, 200);
     setTimeout(() => {
@@ -49,4 +48,8 @@ window.addEventListener("load", () => {
   } else {
     listCon.log("the current page is not login page");
   }
+};
+
+window.addEventListener("load", () => {
+  timeToHideGuestBtn();
 });
